8db983adb1763dd1dc404751c8e390badda6d340
moritzmessner <moritz.messner@ffuf.de>
2025-09-29 14:12:04 +0200

    Add example JSON files for themed designs and update app_config.dart to reference new JSON URL

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,7 +2,7 @@
 class AppConfig {
   // Configure your JSON URL here
   // Set to null to use only local sample data
-  static const String? jsonUrl = 'https://github.com/moritzmessneroqds/poc_json/blob/main/simple_test.json';
+  static const String? jsonUrl = 'https://raw.githubusercontent.com/moritzmessneroqds/poc_json/main/example2.json';
   
   // Alternative: Use local sample data only
   // static const String? jsonUrl = null;
